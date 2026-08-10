101e48ebb6cabed34cce5cb4a2a483962e346681
anerti <anerti@endeavouros.localdomain>
2026-08-10 17:26:28 +0200

    feat: add JUser JPA entity

---
+++ src/main/java/com/techindna/anerti/repository/model/JUser.java
@@ -0,0 +1,61 @@
+package com.techindna.anerti.repository.model;
+
+import com.techindna.anerti.entity.enums.UserRole;
+import jakarta.persistence.*;
+import java.time.Instant;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.type.SqlTypes;
+
+@Entity
+@Table(name = "\"user\"")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class JUser {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(nullable = false, unique = true, length = 50)
+  private String username;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(name = "first_name", nullable = false, length = 100)
+  private String firstName;
+
+  @Column(name = "last_name", nullable = false, length = 100)
+  private String lastName;
+
+  @Column(nullable = false, unique = true, length = 100)
+  private String email;
+
+  @Column(nullable = false)
+  @Builder.Default
+  private Boolean verified = false;
+
+  @JdbcTypeCode(SqlTypes.NAMED_ENUM)
+  @Column(name = "role", nullable = false)
+  @Builder.Default
+  private UserRole role = UserRole.CUSTOMER;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false)
+  private Instant createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at")
+  private Instant updatedAt;
+}
