fa38a868e8f529db4ff33519e997e47ee806245d
anerti <anerti@endeavouros.localdomain>
2026-08-10 17:26:27 +0200

    feat: add auth endpoints (register, login, resend-link, verify)

---
+++ src/main/java/com/techindna/anerti/endpoint/rest/controller/AuthController.java
@@ -0,0 +1,53 @@
+package com.techindna.anerti.endpoint.rest.controller;
+
+import com.techindna.anerti.dto.LoginInput;
+import com.techindna.anerti.dto.MessageBody;
+import com.techindna.anerti.dto.RegisterInput;
+import com.techindna.anerti.dto.VerifyRegistrationResponse;
+import com.techindna.anerti.service.AuthService;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+
+  private final AuthService authService;
+
+  @PostMapping("/register")
+  public ResponseEntity<MessageBody> register(
+      @RequestBody RegisterInput request, HttpServletRequest servletRequest) {
+    return ResponseEntity.status(HttpStatus.ACCEPTED)
+        .body(authService.register(request, servletRequest));
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<MessageBody> login(
+      @RequestBody LoginInput request, HttpServletRequest servletRequest) {
+    return ResponseEntity.status(HttpStatus.ACCEPTED)
+        .body(authService.login(request, servletRequest));
+  }
+
+  @PostMapping("/resend-link")
+  public ResponseEntity<MessageBody> resendVerificationLink(
+      @RequestParam String email, HttpServletRequest servletRequest) {
+    return ResponseEntity.status(HttpStatus.ACCEPTED)
+        .body(authService.resendVerificationLink(email, servletRequest));
+  }
+
+  @GetMapping("/verification/{token}")
+  public ResponseEntity<VerifyRegistrationResponse> verify(@PathVariable UUID token) {
+    return ResponseEntity.status(HttpStatus.OK).body(authService.verify(token));
+  }
+}
