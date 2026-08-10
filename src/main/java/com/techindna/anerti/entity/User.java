c3d03d1c5f0024f59981b0acbc0acb745b591ec6
anerti <anerti@endeavouros.localdomain>
2026-08-10 17:26:28 +0200

    feat: add User entity

---
+++ src/main/java/com/techindna/anerti/entity/User.java
@@ -0,0 +1,15 @@
+package com.techindna.anerti.entity;
+
+import com.techindna.anerti.entity.enums.UserRole;
+import java.time.Instant;
+import java.util.UUID;
+
+public record User(
+    UUID id,
+    String username,
+    String firstName,
+    String lastName,
+    String email,
+    UserRole role,
+    Instant createdAt,
+    Instant updatedAt) {}
