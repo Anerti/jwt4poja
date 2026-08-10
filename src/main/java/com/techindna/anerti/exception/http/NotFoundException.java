880e92dd5808503acfac60db258a8b21010b747b
anerti <anerti@endeavouros.localdomain>
2026-08-10 17:26:28 +0200

    feat: add NotFoundException

---
+++ src/main/java/com/techindna/anerti/exception/http/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.techindna.anerti.exception.http;
+
+public class NotFoundException extends RuntimeException {
+
+  public NotFoundException(String message) {
+    super(message);
+  }
+}
