0ce9a606f4efb6f1eb4850313bd1ebc8e7c663a2
anerti <anerti@endeavouros.localdomain>
2026-08-10 17:26:28 +0200

    test: add EnvConf test config

---
+++ src/test/java/com/techindna/anerti/conf/EnvConf.java
@@ -0,0 +1,28 @@
+package com.techindna.anerti.conf;
+
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
+
+public class EnvConf {
+
+  static final PostgreSQLContainer POSTGRES =
+      new PostgreSQLContainer("postgres:16-alpine").withInitScript("test-init.sql");
+  static final GenericContainer<?> REDIS =
+      new GenericContainer<>("redis:7-alpine").withExposedPorts(6379);
+
+  static {
+    POSTGRES.start();
+    REDIS.start();
+  }
+
+  void configureProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
+    registry.add("spring.datasource.username", POSTGRES::getUsername);
+    registry.add("spring.datasource.password", POSTGRES::getPassword);
+    registry.add("spring.jpa.properties.hibernate.default_schema", () -> "jwt4poja_app");
+    registry.add(
+        "spring.data.redis.url",
+        () -> "redis://" + REDIS.getHost() + ":" + REDIS.getMappedPort(6379));
+  }
+}
