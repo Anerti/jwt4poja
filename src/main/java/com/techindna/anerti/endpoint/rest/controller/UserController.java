0e9e388b5268ab6f67303d0bf7a64df86fa1ee9b
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-08-11 13:45:58 +0300

    feat: add GET /users/{userId} endpoint

--- src/main/java/com/techindna/anerti/endpoint/rest/controller/UserController.java
+++ src/main/java/com/techindna/anerti/endpoint/rest/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.techindna.anerti.endpoint.rest.controller;
 
 import com.techindna.anerti.dto.UserListResponse;
+import com.techindna.anerti.entity.User;
 import com.techindna.anerti.entity.enums.SortDirection;
 import com.techindna.anerti.service.UserService;
+import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +27,12 @@ public class UserController {
       @RequestParam(defaultValue = "1") int page,
       @RequestParam(defaultValue = "10") int size,
       @RequestParam(defaultValue = "ASC") SortDirection sort) {
-    return ResponseEntity.ok(userService.listUsers(search, page, size, sort));
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(userService.listUsers(search, page, size, sort));
+  }
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<User> getUser(@PathVariable UUID userId) {
+    return ResponseEntity.status(HttpStatus.OK).body(userService.getUser(userId));
   }
 }
