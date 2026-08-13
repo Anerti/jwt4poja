2e0ba6a79af05f752669008ca2000a8f14e37920
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-08-13 19:42:22 +0300

    refactor: remove /users endpoints

--- src/main/java/com/techindna/anerti/dto/UserListResponse.java
+++
@@ -1,6 +0,0 @@
-package com.techindna.anerti.dto;
-
-import com.techindna.anerti.entity.User;
-import java.util.List;
-
-public record UserListResponse(List<User> data, Meta meta) {}
--- src/main/java/com/techindna/anerti/mapper/UserMapper.java
+++ src/main/java/com/techindna/anerti/mapper/UserMapper.java
@@ -1,7 +1,6 @@
 package com.techindna.anerti.mapper;
 
 import com.techindna.anerti.dto.RegisterInput;
-import com.techindna.anerti.dto.UpdateUserInput;
 import com.techindna.anerti.entity.User;
 import com.techindna.anerti.entity.enums.UserRole;
 import com.techindna.anerti.repository.model.JUser;
@@ -20,18 +19,6 @@ public class UserMapper {
         .verified(false)
         .role(UserRole.CUSTOMER)
         .build();
-  }
-
-  public void applyUpdate(JUser jUser, UpdateUserInput input) {
-    if (input.username() != null) {
-      jUser.setUsername(input.username().strip());
-    }
-    if (input.firstName() != null) {
-      jUser.setFirstName(input.firstName().strip());
-    }
-    if (input.lastName() != null) {
-      jUser.setLastName(input.lastName().strip());
-    }
   }
 
   public User toDomain(JUser jUser) {
