bcb84e03143c4faa7ef2c22e00c8ad714c62f192
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-08-13 22:30:59 +0300

    feat: add validateCreateTeacher

--- src/main/java/com/techindna/anerti/validator/UserValidator.java
+++ src/main/java/com/techindna/anerti/validator/UserValidator.java
@@ -1,5 +1,6 @@
 package com.techindna.anerti.validator;
 
+import com.techindna.anerti.dto.CreateTeacherInput;
 import com.techindna.anerti.dto.LoginInput;
 import com.techindna.anerti.exception.http.UnprocessableContentException;
 import lombok.AllArgsConstructor;
@@ -10,6 +11,15 @@ import org.springframework.stereotype.Component;
 public class UserValidator {
 
   private final DataValidator dataValidator;
+
+  public void validateCreateTeacher(CreateTeacherInput request) {
+    dataValidator.validateUsername(request.username());
+    dataValidator.checkPasswordSecurityLevel(request.password());
+    dataValidator.validateName("firstName", request.firstName());
+    dataValidator.validateName("lastName", request.lastName());
+    dataValidator.validateEmail("email", request.email());
+    dataValidator.validateRef(request.ref());
+  }
 
   public void validateLogin(LoginInput request) {
     if (request.email() != null && !request.email().isBlank()) {
