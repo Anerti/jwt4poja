9ea6af6daec586ffeb1447a5f5b04186847df319
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-08-14 15:04:19 +0300

    feat: add StudentInheritance dto

---
+++ src/main/java/com/techindna/anerti/dto/StudentInheritance.java
@@ -0,0 +1,17 @@
+package com.techindna.anerti.dto;
+
+import com.techindna.anerti.repository.enums.LearningPath;
+import com.techindna.anerti.repository.enums.Level;
+import com.techindna.anerti.repository.enums.StudentStatus;
+import java.time.Instant;
+import java.util.UUID;
+
+public record StudentInheritance(
+    UUID id,
+    String ref,
+    Instant joinedAt,
+    Level level,
+    LearningPath learningPath,
+    StudentStatus studentStatus,
+    Integer graduationYear,
+    String className) {}
