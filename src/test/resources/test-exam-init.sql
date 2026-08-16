a51083b77087aab0d06179314cbf1ea69e99ab2b
Ervin ANDRIANOMBANA <e.andrianombana@protonmail.com>
2026-08-16 10:55:28 +0300

    test: add exam test init script

---
+++ src/test/resources/test-exam-init.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA IF NOT EXISTS jwt4poja_app;
+
+CREATE TABLE IF NOT EXISTS jwt4poja_app.exam (
+    id            UUID           PRIMARY KEY DEFAULT gen_random_uuid(),
+    course_id     UUID           NOT NULL REFERENCES jwt4poja_app.course(id) ON DELETE CASCADE,
+    coefficient   NUMERIC(10, 2) NOT NULL,
+    academic_year VARCHAR(10)    NOT NULL,
+    date          TIMESTAMPTZ    NOT NULL,
+    created_at    TIMESTAMPTZ    NOT NULL DEFAULT now(),
+    updated_at    TIMESTAMPTZ
+);
